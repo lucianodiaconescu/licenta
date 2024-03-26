42841663c2d23c9fba9ef941a152353d6e21fc8d
lucianodiaconescu <134278239+lucianodiaconescu@users.noreply.github.com>
2024-03-26 00:56:37 -1200

    Delete src directory

--- src/main/java/corectare/automata/lucrare_licenta/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package corectare.automata.lucrare_licenta.repositories;
-
-import corectare.automata.lucrare_licenta.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
